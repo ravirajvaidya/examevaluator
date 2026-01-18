9253b91b4a534fe1508cc35d1bb6c0043e7a5bb3
ravirajvaidya <ravirajvaidya06@gmail.com>
2026-01-18 16:03:14 +0530

    Added EvaluationPage component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import Info from './components/Info';
+import EvaluationPage from './components/EvaluationPage';
+// import Info from './components/Info';
 // import ExcelEvaluator from './components/ExcelEvaluator';
-import NewLayout from './components/NewLayout';
 
 function App() {
   return (
     <div className="App">
       {/* <Info></Info> */}
       {/* <ExcelEvaluator></ExcelEvaluator> */}
-      <NewLayout></NewLayout>
+      <EvaluationPage></EvaluationPage>
     </div>
   );
 }
