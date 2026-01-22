7bf17ea546bea344fd3f3310826d3e31287bda89
ravirajvaidya <ravirajvaidya06@gmail.com>
2026-01-22 13:18:09 +0530

    Login Module implemented

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_PUBLISHABLE_KEY;
 
 export default function LoginPage(props) {
     const navigate = useNavigate();
-
+    const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
     const [formData, setFormData] = useState({
         email: "",
         password: ""
@@ -31,10 +35,20 @@ export default function LoginPage(props) {
             console.log("Login Data:", formData);
 
             // TODO: Supabase / API login
-            // await supabase.auth.signInWithPassword({
-            //   email: formData.email,
-            //   password: formData.password,
-            // });
+            const { data, error } = await supabase.auth.signInWithPassword({
+                email: formData.email,
+                password: formData.password,
+            });
+
+            if (!error) {
+                console.log("Login success:", data);
+                alert("Login successful!");
+                navigate("/dashboard"); // change to /dashboard if needed
+                setLoading(false);
+            }
+            else {
+                throw (error);
+            }
 
         } catch (err) {
             setError("Invalid email or password");
